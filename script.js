0bd039e2a0b4047640ca54b4809e351ae89e58be
JorrynPanjasuran <163964500+JorrynPanjasuran@users.noreply.github.com>
2024-11-04 14:22:25 +0200

    content

--- script.js
+++ script.js
@@ -67,4 +67,27 @@ function updateProgressBar() {
 
 //Scrolls on bottom
 
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                "color-1": "hsl(var(--color-1))",
+                "color-2": "hsl(var(--color-2))",
+                "color-3": "hsl(var(--color-3))",
+                "color-4": "hsl(var(--color-4))",
+                "color-5": "hsl(var(--color-5))",
+            },
+            animation: {
+                rainbow: "rainbow 2s infinite linear",
+            },
+            keyframes: {
+                rainbow: {
+                    "0%": { backgroundPosition: "0%" },
+                    "100%": { backgroundPosition: "200%" },
+                },
+            },
+        },
+    },
+};
 
